e6b8155e8a172d5d164ec5a2e52da9df6e832e51
taufiq270499 <taupiqurahman99@gmail.com>
2021-10-07 02:59:57 +0700

    first commit

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+//contoh tipe data integer
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "binary :";
+var_dump(0b111111);
+
+
+//floating point
+
+echo "Floating Point: ";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation : (1.7 x 1000): ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E Notation minus : (1.7 x 0.001): ";
+var_dump(1.7e-3);
+
+echo "Underscore di floating point: ";
+var_dump(1123.123);
---
+++ TipeDataString.php
@@ -0,0 +1,20 @@
+<?php 
+
+//single quote
+
+echo 'Name : ';
+echo 'Taupiqurahman';
+echo "\n";
+
+//double quote
+
+echo "Nama : ";
+echo "Taupiqurahman\n";
+
+//
+
+echo <<<EKO
+'Selamat Belajar PHP
+Sekarang kita belajar tipe data string dengan
+menggunakan heredoc'
+EKO;
